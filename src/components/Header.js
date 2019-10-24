74b16d397dc53dffa695401e83bec87dd5e14a82
Jone Træet <jone.traet@hotmail.com>
2019-10-24 12:00:16 +0200

    første

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Header.scss";
+
+function Header() {
+  return (
+    <ul class="navbar">
+      <li>Home</li>
+      <li>About us</li>
+      <li>Portfolio</li>
+    </ul>
+  );
+}
+
+export default Header;
---
+++ src/components/MainContent.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function MainContent() {
+  return (
+    <div>
+    <h1>Jone Træet</h1>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+        Sed diam arcu, pulvinar vitae commodo sed, 
+        scelerisque eget tellus. Integer pharetra egestas tortor,
+         in finibus erat interdum et. Curabitur sed laoreet tellus. 
+         Quisque bibendum aliquet pharetra. Nunc non ultrices tortor. 
+         Pellentesque eget lacinia nunc. Pellentesque placerat dictum.
+    </p>
+    <ul>
+      <li>Los Angeles</li>
+      <li>Italia</li>
+      <li>Maldivene</li>
+    </ul>
+    </div>
+  );
+}
+
+export default MainContent;
